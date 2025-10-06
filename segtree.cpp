25b11399b5bb5b270ad97ed73ab08e4687a017ba
Jairo <154546457+JairoGonzaga@users.noreply.github.com>
2025-10-06 15:20:52 -0300

    Segtree_sum

---
+++ segtree.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+class segtree
+{
+private:
+    int* vetor;
+public:
+    segtree(int N){
+        vetor = new int[N*4];
+    }
+    ~segtree(){
+        delete[] vetor;
+    }
+    void build(ll vet[], ll esq, ll direita, ll v){
+        if(esq == direita){
+            vetor[v] == vet[esq]; 
+        }else{
+            int newindc = (esq+direita)/2;
+            build(vet,esq,newindc,v*2);
+            build(vet,newindc+1,direita,v*2+1);
+            vetor[v]=vetor[v*2]+vetor[v*2+1];
+
+        }
+    }
+    ll sum(ll v, ll esq, ll direita, ll l, ll r){
+
+        if(l>r) return 0;
+        if(l==esq && r == direita) return vetor[v];
+        ll ni = (esq+direita)/2;
+        return sum(v*2, esq, ni, l, min(r, ni)) + sum(v*2+1, ni+1, direita, max(l, ni+1), r);
+        
+    }
+    void update(int v, int esq, int direita, int pos, int new_val) {
+    if (esq == direita) {
+        vetor[v] = new_val;
+    } else {
+        int tm = (esq + direita) / 2;
+        if (pos <= tm)
+            update(v*2, esq, tm, pos, new_val);
+        else
+            update(v*2+1, tm+1, direita, pos, new_val);
+        vetor[v] = vetor[v*2] + vetor[v*2+1];
+    }
+}
+};
+
